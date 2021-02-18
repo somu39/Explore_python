a1e0d4bf586a826f000250a2f95610328dfa5e36
somu39 <someshdas39@gmail.com>
2021-02-18 17:46:37 +0530

    commit

--- Test_01.py
+++ Test_01.py
@@ -1,1 +1,12 @@
 print("hello warld iam somesh das")
+var = 25
+char_name = 'somesh'
+char_age = 24.5
+
+print("my name is", char_name, "my age is", char_age, ".")
+
+
+print(char_name.upper())
+char_name=char_name.upper()
+print(char_name)
+
