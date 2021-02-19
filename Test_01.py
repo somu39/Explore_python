ef06d0964b5d53a9079e5c9f34ce2a397c3311c6
somu39 <someshdas39@gmail.com>
2021-02-19 19:53:46 +0530

    commit

--- Test_01.py
+++ Test_01.py
@@ -3,16 +3,5 @@ var = 25
 char_name = 'somesh'
 char_age = 24.5
 
-print("my name is", char_name, "my age is", char_age, ".")
-
-
-print(char_name.upper())
-char_name=char_name.upper()
-print(char_name)
-
-vt=35.535525
-print(round(vt))
-
-print(min(1,2,5,6,8,0))
-li=[6,15,96]
-print(max(li))
+my_string='somesh das'
+print(my_string[1:6])
