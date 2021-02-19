056a881b11104405f7cfd525000d2eb81ac4f84e
somu39 <someshdas39@gmail.com>
2021-02-19 21:38:48 +0530

    commit

--- Test_01.py
+++ Test_01.py
@@ -2,7 +2,7 @@ dob = '22/07/1996'
 print(dob)
 
 print(dob.split())
-l1 = len(dob)
+l1 = len(dob) #improtent
 
 print(dob[0:2])
 print(dob[3:5])
@@ -11,8 +11,4 @@ print(dob[6:10])
 dd = int(dob[0:2])
 mm = int(dob[3:5])
 yy = int(dob[6:10])
-
-num = '02'
-num2 = int(num)
-
 
